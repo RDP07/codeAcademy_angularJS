402e29f7e323aba31c67d7230d0687dcd1be6abc
RDP <rdupaw@gmail.com>
2017-03-07 12:51:02 -0600

    Delete $scope.product object

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,9 +1,4 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Books We Recommend';
   $scope.promo = 'Start your next reading adventure!';
-  $scope.product = {
-    name: 'The Book of Trees',
-    price: 19,
-    pubdate: new Date('2014', '03', '08')
-                   };
 }]);
