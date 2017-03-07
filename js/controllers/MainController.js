0cf33aa002c20b7d1d8fdce04ff033dd98dd708a
RDP <rdupaw@gmail.com>
2017-03-07 12:43:14 -0600

    Add pubdate to $scope.product

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -3,6 +3,7 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.promo = 'Start your next reading adventure!';
   $scope.product = {
     name: 'The Book of Trees',
-    price: 19
+    price: 19,
+    pubdate: new Date('2014', '03', '08')
                    };
 }]);
