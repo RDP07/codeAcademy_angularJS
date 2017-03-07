18754e7095232b0d81037170ef0ec393ae52dc97
RDP <rdupaw@gmail.com>
2017-03-07 13:00:18 -0600

    Add $scope.products with array of two books

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,18 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Books We Recommend';
   $scope.promo = 'Start your next reading adventure!';
+  $scope.products = [
+                      {
+                      	name: 'The Book of Trees',
+                      	price: 19,
+                      	pubdate: new Date('2014', '03', '08'),
+                      	cover: 'img/the-book-of-trees.jpg'
+                      },
+                      {
+                      	name: "Program or be Programmed",
+                      	price: 8,
+                      	pubdate: new Date('2013', '08', '01'),
+                      	cover: 'img/program-or-be-programmed.jpg'
+                      }
+                    ];
 }]);
