bb6b297005fb539b016075f5221f305d648be03f
RDP <rdupaw@gmail.com>
2017-03-07 19:03:00 -0600

    Add likes property to products

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -6,25 +6,29 @@ app.controller('MainController', ['$scope', function($scope) {
                       	name: 'The Book of Trees',
                       	price: 19,
                       	pubdate: new Date('2014', '03', '08'),
-                      	cover: 'img/the-book-of-trees.jpg'
+                      	cover: 'img/the-book-of-trees.jpg',
+                      	likes: 0
                       },
                       {
                       	name: "Program or be Programmed",
                       	price: 8,
                       	pubdate: new Date('2013', '08', '01'),
-                      	cover: 'img/program-or-be-programmed.jpg'
+                      	cover: 'img/program-or-be-programmed.jpg',
+                      	likes: 0
                       },
                       {
                       	name: 'HTML & CSS: Design and Build Websites',
                       	price: 8,
                       	pubdate: new Date('2011','11', '08'),
-                      	cover: 'img/html-and-css.jpg'
+                      	cover: 'img/html-and-css.jpg',
+                      	likes: 0
                       },
                       {
                       	name: 'JavaScript & JQuery: Interactive Front-End Web Development',
                       	price: 28,
                       	pubdate: new Date('2014', '06', '30'),
-                      	cover: "img/javascript-and-jquery.jpg"
+                      	cover: "img/javascript-and-jquery.jpg",
+                      	likes: 0
                       }
                     ];
 }]);
